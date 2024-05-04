914a47d2b2f74da36940988c16b85982c45c8173
Nguyễn Văn Huy <huyhaithanh51@gmail.com>
2024-05-04 15:41:26 +0700

    change class user

--- DaNangTourism/DaNangTourism.Server/obj/Debug/net8.0/DaNangTourism.Server.AssemblyInfo.cs
+++ DaNangTourism/DaNangTourism.Server/obj/Debug/net8.0/DaNangTourism.Server.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DaNangTourism.Server")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ee2c10861622baa111d10ae5db07607eb5bfb313")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f30f340240e0ffc24fc56d59761c6ae26accbc28")]
 [assembly: System.Reflection.AssemblyProductAttribute("DaNangTourism.Server")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DaNangTourism.Server")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
