20c27478a076e08fa2eb885f020efd5f30525014
ldb-258 <102220094@sv1.dut.udn.vn>
2024-05-14 15:15:08 +0700

    update: fe

---
+++ DaNangTourism/danangtourism.client/src/pages/schedule/Schedule.tsx
@@ -0,0 +1,127 @@
+import { useParams } from 'react-router-dom'
+import { ScheduleType } from '../../types/schedule.types'
+import { useEffect, useState } from 'react'
+import axios from 'axios'
+import { Button, Loader } from '../../components'
+import PageNotFound from '../PageNotFound'
+import { timeAgo } from '../../utils/TimeFormatters'
+import ScheduleOverview from './ScheduleOverview'
+import ScheduleDay from './ScheduleDay'
+import { PiGearFill, PiMapPinLineFill } from 'react-icons/pi'
+import AddDestinationModal from './AddDestinationModal'
+import SetupModal from './SetupModal'
+
+const Schedule: React.FC = () => {
+	const { id } = useParams()
+	const [schedule, setSchedule] = useState<ScheduleType | undefined>(undefined)
+	const [loading, setLoading] = useState(true)
+	const [isAddModalOpen, setIsAddModalOpen] = useState(false)
+	const [isSetupModalOpen, setIsSetupModalOpen] = useState(false)
+	const getSchedule = async (id: string) => {
+		try {
+			setSchedule(undefined)
+			const response = await axios.get(`/api/schedule/id-${id}.json`)
+			// simulate delay
+			await new Promise((resolve) => setTimeout(resolve, 1000))
+			setSchedule(response.data.data)
+		} catch (error) {
+			console.error(error)
+		}
+		setLoading(false)
+	}
+
+	useEffect(() => {
+		getSchedule(id ?? '')
+	}, [id])
+
+	useEffect(() => {
+		document.title = (schedule?.title ?? 'Schedules') + ' | Da Nang Explore'
+	}, [schedule])
+
+	if (loading)
+		return (
+			<div className="mx-auto mt-12 flex min-h-screen items-center justify-center xl:max-w-screen-xl">
+				<Loader />
+			</div>
+		)
+	else if (!schedule) return <PageNotFound />
+	return (
+		<div className="relative mx-auto min-h-screen xl:max-w-screen-xl">
+			<div className="w-full pb-5 pt-[64px] text-txtCol-1">
+				<div className="flex w-full flex-col items-start justify-start gap-2">
+					<h2 className="my-2 text-3xl font-bold">{schedule.title}</h2>
+					<div className="flex items-center gap-2 text-base">
+						<img
+							className="h-6 w-6 rounded-full object-cover"
+							src={schedule.author.avatar}
+							alt="author-avatar"
+						/>
+						<p>by {schedule.author.name}</p>
+						<span className="mx-1 h-[6px] w-[6px] rounded-full bg-txtCol-2"></span>
+						<h4 className="font-semibold">Last updated:</h4>
+						<p>{timeAgo(schedule.updated_at)}</p>
+					</div>
+					<p className="text-base">{schedule.description}</p>
+				</div>
+				<div className="item-start mt-5 flex w-full gap-16">
+					<div className="flex flex-1 flex-col items-start gap-4">
+						<Button
+							className="ml-[100px] h-9 w-[172px] bg-secondary-2 font-semibold text-white hover:bg-secondary-1 "
+							onClick={() => setIsAddModalOpen(true)}
+						>
+							<PiMapPinLineFill className="text-lg" />
+							Add destination
+						</Button>
+						{schedule.details.map((day, index) => (
+							<ScheduleDay
+								className="mb-2 w-full"
+								key={index}
+								scheduleDay={day}
+							/>
+						))}
+					</div>
+					<div className="flex w-[300px] flex-col items-end gap-4">
+						<Button
+							className="h-9 w-[100px] bg-primary-2 font-semibold text-white hover:bg-primary-1"
+							onClick={() => setIsSetupModalOpen(true)}
+						>
+							<PiGearFill className="text-lg" />
+							Set up
+						</Button>
+						<ScheduleOverview
+							className="w-full"
+							numbOfDes={schedule.details.reduce((total, detail) => {
+								return total + detail.destinations.length
+							}, 0)}
+							totalTime={schedule.details.length}
+							totalBudget={schedule.details.reduce((totalBudget, detail) => {
+								return (
+									totalBudget +
+									detail.destinations.reduce(
+										(total, destination) => total + destination.budget,
+										0,
+									)
+								)
+							}, 0)}
+						/>
+					</div>
+				</div>
+			</div>
+			{isAddModalOpen && (
+				<AddDestinationModal
+					className="fixed left-0 top-0 z-10 h-screen w-screen"
+					onCancel={() => setIsAddModalOpen(false)}
+				/>
+			)}
+			{isSetupModalOpen && (
+				<SetupModal
+					scheduleId={schedule.id}
+					className="fixed left-0 top-0 z-10 h-screen w-screen"
+					onCancel={() => setIsSetupModalOpen(false)}
+				/>
+			)}
+		</div>
+	)
+}
+
+export default Schedule
---
+++ DaNangTourism/danangtourism.client/tailwind.config.js
@@ -0,0 +1,48 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+	content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
+	theme: {
+		extend: {
+			fontFamily: {
+				sans: ['Poppins', 'sans-serif'],
+				lora: ['Lora', 'serif'],
+				oxygenMono: ['Oxygen Mono', 'monospace'],
+			},
+			colors: {
+				primary: {
+					1: '#2898C8',
+					2: '#64B8DC',
+					3: '#64CCDC',
+				},
+				secondary: {
+					0: '#49ad91',
+					1: '#52B69A',
+					2: '#76C893',
+				},
+				tertiary: {
+					1: '#E75A51',
+					2: '#ee685e',
+					3: '#ff9f86',
+				},
+				bgCol: {
+					1: '#F6FAF9',
+					2: '#FFFFFF',
+				},
+				txtCol: {
+					1: '#0C0C20',
+					2: '#555555',
+					3: '#999999',
+					4: '#d3d3d3',
+				},
+				borderCol: {
+					1: '#D4D4D4',
+					2: '#F4F4F4',
+				},
+			},
+			boxShadow: {
+				custom: '2px 2px 8px 0px rgba(0, 0, 0, 0.25)',
+			},
+		},
+	},
+	plugins: [],
+}
