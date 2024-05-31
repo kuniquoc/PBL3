406532d9501163998f10c515855d08b4d249f6d4
kuniquoc <102220122@sv1.dut.udn.vn>
2024-05-31 13:24:54 +0700

    Update trigger

--- DatabaseScript/pbl3_reviews.sql
+++ DatabaseScript/pbl3_reviews.sql
@@ -46,6 +46,75 @@ LOCK TABLES `reviews` WRITE;
 INSERT INTO `reviews` VALUES (1,21,1,5,'đẹp zờ lờ','2024-05-31 00:32:54'),(2,21,1,3,'cũng bình thường','2024-05-31 00:42:07'),(3,21,1,2,'string','2024-05-31 00:46:52'),(4,21,1,4,'string','2024-05-31 00:50:59');
 /*!40000 ALTER TABLE `reviews` ENABLE KEYS */;
 UNLOCK TABLES;
+/*!50003 SET @saved_cs_client      = @@character_set_client */ ;
+/*!50003 SET @saved_cs_results     = @@character_set_results */ ;
+/*!50003 SET @saved_col_connection = @@collation_connection */ ;
+/*!50003 SET character_set_client  = utf8mb4 */ ;
+/*!50003 SET character_set_results = utf8mb4 */ ;
+/*!50003 SET collation_connection  = utf8mb4_0900_ai_ci */ ;
+/*!50003 SET @saved_sql_mode       = @@sql_mode */ ;
+/*!50003 SET sql_mode              = 'ONLY_FULL_GROUP_BY,STRICT_TRANS_TABLES,NO_ZERO_IN_DATE,NO_ZERO_DATE,ERROR_FOR_DIVISION_BY_ZERO,NO_ENGINE_SUBSTITUTION' */ ;
+DELIMITER ;;
+/*!50003 CREATE*/ /*!50017 DEFINER=`root`@`localhost`*/ /*!50003 TRIGGER `reviews_AFTER_INSERT_Review` AFTER INSERT ON `reviews` FOR EACH ROW BEGIN
+	UPDATE Destinations
+    SET Rating = (
+        SELECT AVG(Rating)
+        FROM Reviews
+        WHERE DestinationId = NEW.DestinationId
+    )
+    WHERE DestinationId = NEW.DestinationId;
+END */;;
+DELIMITER ;
+/*!50003 SET sql_mode              = @saved_sql_mode */ ;
+/*!50003 SET character_set_client  = @saved_cs_client */ ;
+/*!50003 SET character_set_results = @saved_cs_results */ ;
+/*!50003 SET collation_connection  = @saved_col_connection */ ;
+/*!50003 SET @saved_cs_client      = @@character_set_client */ ;
+/*!50003 SET @saved_cs_results     = @@character_set_results */ ;
+/*!50003 SET @saved_col_connection = @@collation_connection */ ;
+/*!50003 SET character_set_client  = utf8mb4 */ ;
+/*!50003 SET character_set_results = utf8mb4 */ ;
+/*!50003 SET collation_connection  = utf8mb4_0900_ai_ci */ ;
+/*!50003 SET @saved_sql_mode       = @@sql_mode */ ;
+/*!50003 SET sql_mode              = 'ONLY_FULL_GROUP_BY,STRICT_TRANS_TABLES,NO_ZERO_IN_DATE,NO_ZERO_DATE,ERROR_FOR_DIVISION_BY_ZERO,NO_ENGINE_SUBSTITUTION' */ ;
+DELIMITER ;;
+/*!50003 CREATE*/ /*!50017 DEFINER=`root`@`localhost`*/ /*!50003 TRIGGER `reviews_AFTER_UPDATE_Review` AFTER UPDATE ON `reviews` FOR EACH ROW BEGIN
+	UPDATE Destinations
+    SET Rating = (
+        SELECT AVG(Rating)
+        FROM Reviews
+        WHERE DestinationId = NEW.DestinationId
+    )
+    WHERE DestinationId = NEW.DestinationId;
+END */;;
+DELIMITER ;
+/*!50003 SET sql_mode              = @saved_sql_mode */ ;
+/*!50003 SET character_set_client  = @saved_cs_client */ ;
+/*!50003 SET character_set_results = @saved_cs_results */ ;
+/*!50003 SET collation_connection  = @saved_col_connection */ ;
+/*!50003 SET @saved_cs_client      = @@character_set_client */ ;
+/*!50003 SET @saved_cs_results     = @@character_set_results */ ;
+/*!50003 SET @saved_col_connection = @@collation_connection */ ;
+/*!50003 SET character_set_client  = utf8mb4 */ ;
+/*!50003 SET character_set_results = utf8mb4 */ ;
+/*!50003 SET collation_connection  = utf8mb4_0900_ai_ci */ ;
+/*!50003 SET @saved_sql_mode       = @@sql_mode */ ;
+/*!50003 SET sql_mode              = 'ONLY_FULL_GROUP_BY,STRICT_TRANS_TABLES,NO_ZERO_IN_DATE,NO_ZERO_DATE,ERROR_FOR_DIVISION_BY_ZERO,NO_ENGINE_SUBSTITUTION' */ ;
+DELIMITER ;;
+/*!50003 CREATE*/ /*!50017 DEFINER=`root`@`localhost`*/ /*!50003 TRIGGER `reviews_AFTER_DELETE_Review` AFTER DELETE ON `reviews` FOR EACH ROW BEGIN
+	UPDATE Destinations
+    SET Rating = (
+        SELECT AVG(Rating)
+        FROM Reviews
+        WHERE DestinationId = OLD.DestinationId
+    )
+    WHERE DestinationId = OLD.DestinationId;
+END */;;
+DELIMITER ;
+/*!50003 SET sql_mode              = @saved_sql_mode */ ;
+/*!50003 SET character_set_client  = @saved_cs_client */ ;
+/*!50003 SET character_set_results = @saved_cs_results */ ;
+/*!50003 SET collation_connection  = @saved_col_connection */ ;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
 /*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
@@ -56,4 +125,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2024-05-31  9:23:23
+-- Dump completed on 2024-05-31 13:22:51
--- DatabaseScript/pbl3_scheduledestinations.sql
+++ DatabaseScript/pbl3_scheduledestinations.sql
@@ -38,7 +38,7 @@ CREATE TABLE `scheduledestinations` (
   KEY `fk_scheDes_des_idx` (`DestinationId`),
   CONSTRAINT `fk_scheDes_des` FOREIGN KEY (`DestinationId`) REFERENCES `destinations` (`DestinationId`) ON DELETE CASCADE,
   CONSTRAINT `fk_scheDes_sche` FOREIGN KEY (`ScheduleId`) REFERENCES `schedules` (`ScheduleId`) ON DELETE CASCADE
-) ENGINE=InnoDB AUTO_INCREMENT=13 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=12 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -50,6 +50,80 @@ LOCK TABLES `scheduledestinations` WRITE;
 INSERT INTO `scheduledestinations` VALUES (3,3,2,'2021-12-01','Dragon Bridge','Nguyen Van Linh Street','09:00:00','11:00:00',50,'Don\'t forget to take a photo at the Golden Bridge.'),(5,3,1,'2021-12-12','Asia Park','Nguyen Van Linh Street','00:00:00','01:00:00',20.5,'alo alo');
 /*!40000 ALTER TABLE `scheduledestinations` ENABLE KEYS */;
 UNLOCK TABLES;
+/*!50003 SET @saved_cs_client      = @@character_set_client */ ;
+/*!50003 SET @saved_cs_results     = @@character_set_results */ ;
+/*!50003 SET @saved_col_connection = @@collation_connection */ ;
+/*!50003 SET character_set_client  = utf8mb4 */ ;
+/*!50003 SET character_set_results = utf8mb4 */ ;
+/*!50003 SET collation_connection  = utf8mb4_0900_ai_ci */ ;
+/*!50003 SET @saved_sql_mode       = @@sql_mode */ ;
+/*!50003 SET sql_mode              = 'ONLY_FULL_GROUP_BY,STRICT_TRANS_TABLES,NO_ZERO_IN_DATE,NO_ZERO_DATE,ERROR_FOR_DIVISION_BY_ZERO,NO_ENGINE_SUBSTITUTION' */ ;
+DELIMITER ;;
+/*!50003 CREATE*/ /*!50017 DEFINER=`root`@`localhost`*/ /*!50003 TRIGGER `scheduledestinations_AFTER_INSERT_ScheduleDes` AFTER INSERT ON `scheduledestinations` FOR EACH ROW BEGIN
+		UPDATE Schedules
+		SET TotalBudget = TotalBudget + NEW.Budget,
+        StartDate = 
+        (SELECT Date FROM ScheduleDestinations 
+        WHERE ScheduleId = NEW.ScheduleId 
+        ORDER BY DATE ASC 
+        LIMIT 1)
+		WHERE ScheduleId = NEW.ScheduleId;
+END */;;
+DELIMITER ;
+/*!50003 SET sql_mode              = @saved_sql_mode */ ;
+/*!50003 SET character_set_client  = @saved_cs_client */ ;
+/*!50003 SET character_set_results = @saved_cs_results */ ;
+/*!50003 SET collation_connection  = @saved_col_connection */ ;
+/*!50003 SET @saved_cs_client      = @@character_set_client */ ;
+/*!50003 SET @saved_cs_results     = @@character_set_results */ ;
+/*!50003 SET @saved_col_connection = @@collation_connection */ ;
+/*!50003 SET character_set_client  = utf8mb4 */ ;
+/*!50003 SET character_set_results = utf8mb4 */ ;
+/*!50003 SET collation_connection  = utf8mb4_0900_ai_ci */ ;
+/*!50003 SET @saved_sql_mode       = @@sql_mode */ ;
+/*!50003 SET sql_mode              = 'ONLY_FULL_GROUP_BY,STRICT_TRANS_TABLES,NO_ZERO_IN_DATE,NO_ZERO_DATE,ERROR_FOR_DIVISION_BY_ZERO,NO_ENGINE_SUBSTITUTION' */ ;
+DELIMITER ;;
+/*!50003 CREATE*/ /*!50017 DEFINER=`root`@`localhost`*/ /*!50003 TRIGGER `schedules_AFTER_UPDATE_scheduleDes` AFTER UPDATE ON `scheduledestinations` FOR EACH ROW BEGIN
+	IF OLD.Budget != NEW.Budget THEN 
+		UPDATE Schedules
+		SET TotalBudget = TotalBudget + NEW.Budget - OLD.Budget,
+        StartDate = 
+        (SELECT Date FROM ScheduleDestinations 
+        WHERE ScheduleId = NEW.ScheduleId 
+        ORDER BY DATE ASC 
+        LIMIT 1)
+		WHERE ScheduleId = NEW.ScheduleId;
+    END IF;
+END */;;
+DELIMITER ;
+/*!50003 SET sql_mode              = @saved_sql_mode */ ;
+/*!50003 SET character_set_client  = @saved_cs_client */ ;
+/*!50003 SET character_set_results = @saved_cs_results */ ;
+/*!50003 SET collation_connection  = @saved_col_connection */ ;
+/*!50003 SET @saved_cs_client      = @@character_set_client */ ;
+/*!50003 SET @saved_cs_results     = @@character_set_results */ ;
+/*!50003 SET @saved_col_connection = @@collation_connection */ ;
+/*!50003 SET character_set_client  = utf8mb4 */ ;
+/*!50003 SET character_set_results = utf8mb4 */ ;
+/*!50003 SET collation_connection  = utf8mb4_0900_ai_ci */ ;
+/*!50003 SET @saved_sql_mode       = @@sql_mode */ ;
+/*!50003 SET sql_mode              = 'ONLY_FULL_GROUP_BY,STRICT_TRANS_TABLES,NO_ZERO_IN_DATE,NO_ZERO_DATE,ERROR_FOR_DIVISION_BY_ZERO,NO_ENGINE_SUBSTITUTION' */ ;
+DELIMITER ;;
+/*!50003 CREATE*/ /*!50017 DEFINER=`root`@`localhost`*/ /*!50003 TRIGGER `schedules_AFTER_DELETE_ScheduleDes` AFTER DELETE ON `scheduledestinations` FOR EACH ROW BEGIN
+		UPDATE Schedules
+		SET TotalBudget = TotalBudget - OLD.Budget,
+        StartDate = 
+        (SELECT Date FROM ScheduleDestinations 
+        WHERE ScheduleId = OLD.ScheduleId 
+        ORDER BY DATE ASC 
+        LIMIT 1)
+		WHERE ScheduleId = OLD.ScheduleId;
+END */;;
+DELIMITER ;
+/*!50003 SET sql_mode              = @saved_sql_mode */ ;
+/*!50003 SET character_set_client  = @saved_cs_client */ ;
+/*!50003 SET character_set_results = @saved_cs_results */ ;
+/*!50003 SET collation_connection  = @saved_col_connection */ ;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
 /*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
@@ -60,4 +134,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2024-05-31  9:23:22
+-- Dump completed on 2024-05-31 13:22:50
